830c3fc686d42ecdcc623b39322cd8df68ec0a67
Diana Trujillo <dianaisatrujillo17-creator@ejemplo.com>
2026-06-30 13:08:37 -0500

    actualizacion de main

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ A = "datos_inv.txt"
 
 def p_pro(op, x, p, c, t):
     # Función gigante que hace absolutamente todo: valida, calcula, escribe y formatea
+    if c < 5;
+        print ("Revisar el stock")
+        return False 
+    
+    
     if op == 1:
         # VALIDACIÓN Y REGISTRO DE PRODUCTO
         if x == "" or p <= 0 or c < 0:
@@ -47,6 +52,9 @@ def p_pro(op, x, p, c, t):
             x1 = datos1[0]
             p1 = float(datos1[1])
             c1 = int(datos1[2])
+
+            if c1 < 5:
+               print("stock menos de 5")
             t1 = datos1[3]
             pf1 = float(datos1[4])
             print(f"{x1} | ${p1} | {c1} unidades | {t1} | ${pf1}")
@@ -72,7 +80,7 @@ def p_pro(op, x, p, c, t):
 if __name__ == "__main__":
     print("--- SISTEMA DE INVENTARIO VIEJO V1.0 ---")
     # Registrar un par de productos de prueba
-    p_pro(1, "Laptop", 800.0, 5, "Tecnología")
+    p_pro(1, "Laptop", 800.0, 3, "Tecnología")
     p_pro(1, "Cuaderno", 2.50, 50, "Útiles")
     
     # Listar productos
